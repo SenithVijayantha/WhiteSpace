a5fb00624097c68875c113b495b830a382789d5d
M.W.S.S. Vijayantha <ssvsenith@gmail.com>
2025-03-04 18:53:41 +0530

    Coded html and css components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
  export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-blue": "#043873",
+        "primary-light-blue": "#4F9CF9",
+        "secondary-yellow": "#FFE492",
+        "secondary-blue": "#A7CEFC",
+        "secondary-white": "#FFFFFF",
+        "secondary-black": "#212529"
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
